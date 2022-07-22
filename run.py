f0315e7b5ffdb04da6d8a92f74eb72febe9f1342
Kneasle <kneasle@gmail.com>
2022-07-22 19:37:10 +0100

    Populate and set layout for spreadsheet

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 import re
-from typing import Dict, List, Tuple
+from openpyxl.styles.fonts import Font
+from openpyxl.workbook import Workbook
+from openpyxl.styles import Alignment
+from openpyxl.utils import get_column_letter
+from typing import Dict, Iterable, List, Tuple
 
 
 BELL_NAMES = "1234567890ETABCD"
@@ -19,6 +23,9 @@ def main():
     for t in touches:
         print(f"{t.length}: {t.call_string} ({t.calling_position_string}, {t.runs} runs)")
     print(f"{len(touches)} touches")
+
+    # Save sheet
+    write_spreadsheet(methods, touches, "Lincoln.xlsx")
 
 
 ###########
@@ -184,6 +191,142 @@ def calling_pos_at(row: str, is_single: bool = False) -> str:
     calling_positions = "LBTFVMWH" if is_single else "LIBFVMWH"
     return calling_positions[row.index(TENOR)]
 
+
+#######################
+# SPREADSHEET WRITING #
+#######################
+
+
+def write_spreadsheet(methods: Dict[str, Method], touches: List[Touch], path: str):
+    # Header looks like:
+    #       A/1    B/2    C/3   D/4    E/5     F/6      G/7     H/8        >=I/9
+    #     +----+--------+-----+-----+------+--------+--------+-------+---------------
+    #   1 |    |        |     |     |      |        |        |       |
+    #     +----+--------+-----+-----+------+--------+--------+-------+---------------
+    #     |    |                                                     |
+    #   2 |    |                    <title>                          |
+    #     |    |                                                     | <methods> ....
+    #     +----+--------+-----+-----+------+--------+--------+-------+
+    #   3 |    |    <made by me>    |      |        | back-  |       |
+    #     +----+--------+-----+-----+ runs + queens + rounds + notes +---------------
+    #   4 |    | length | <calling> |      |        |        |       | <method groups> ...
+    #     +----+--------+-----+-----+------+--------+--------+-------+---------------
+    #     |    |        |           |      |        |        |       |
+    # >=5 |    |        |           |      |        |        |       |
+    #                                ...
+    #                             <touches>
+    #                                ...
+    #     |    |        |           |      |        |        |       |
+    #     +----+--------+-----+-----+------+--------+--------+-------+---------------
+    #     |                                   <note on calls>
+    #     +----+--------+-----+-----+------+--------+--------+-------+---------------
+
+    workbook = Workbook()
+    sheet = workbook.active
+
+    vertical_text = Alignment(text_rotation=90, horizontal="center")
+    centre_text = Alignment(horizontal="center")
+
+    font_family = "Times New Roman"
+    font_size = 10
+
+    # Set default font for all cells
+    for col in range(2, 9 + len(methods) + 1):
+        for row in range(2, 5 + len(touches) + 1):
+            sheet.cell(row, col).font = Font(name=font_family, size=font_size)
+
+    # === TOP-LEFT CORNER ===
+    # title
+    sheet.merge_cells("B2:H2")
+    sheet["B2"] = "50 PPE Touches"
+    sheet["B2"].font = Font(name=font_family, size=font_size * 4, bold=True)
+    sheet["B2"].alignment = Alignment(horizontal="center", vertical="center")
+    # made by me
+    sheet.merge_cells("B3:D3")
+    sheet["B3"] = "Compiled by Ben White-Horne"
+    sheet["B3"].font = Font(name=font_family, size=font_size * 1.6, bold=True)
+    sheet["B3"].alignment = Alignment(horizontal="center", vertical="center")
+    # length/calling
+    sheet["B4"] = "Length"
+    sheet["B4"].alignment = centre_text
+    sheet.merge_cells("C4:D4")
+    sheet["C4"] = "Calling (* for single, . for bob, all near calls)"
+    # music
+    sheet.merge_cells("E3:E4")
+    sheet.merge_cells("F3:F4")
+    sheet.merge_cells("G3:G4")
+    sheet["E3"].alignment = centre_text
+    sheet["E3"] = "Runs"
+    sheet["F3"].alignment = vertical_text
+    sheet["F3"] = "Queens"
+    sheet["G3"].alignment = vertical_text
+    sheet["G3"] = "Backrounds"
+    # notes
+    sheet.merge_cells("H3:H4")
+    sheet["H3"].alignment = centre_text
+    sheet["H3"] = "Notes"
+
+    # === METHODS ===
+    for idx, shorthand in enumerate(methods):
+        column = 9 + idx
+        method = methods[shorthand]
+        # Determine the name
+        name = method.name
+        if not method.name.startswith(shorthand):
+            name += f" ({shorthand})"
+        # Set the cell
+        sheet.merge_cells(start_column=column, start_row=2, end_column=column, end_row=3)
+        cell = sheet.cell(column = column, row = 2)
+        cell.value = name
+        cell.alignment = vertical_text
+
+    # === TOUCHES ===
+    for idx, touch in enumerate(touches):
+        row = 5 + idx
+        # Touch info
+        for col in [2, 4, 5]:
+            sheet.cell(row, col).alignment = centre_text
+        for col in [3, 4]:
+            sheet.cell(row, col).font = Font(name="Fira Code", size=font_size)
+        sheet.cell(row, 2).value = touch.length
+        sheet.cell(row, 3).value = touch.call_string
+        sheet.cell(row, 4).value = touch.calling_position_string
+        sheet.cell(row, 5).value = touch.runs
+        # 6 is Queens
+        # 7 is Backrounds
+        sheet.cell(row, 8).value = touch.notes
+        # Method matrix
+        for meth_idx, shorthand in enumerate(methods):
+            if shorthand in touch.method_counts:
+                cell = sheet.cell(row, 9 + meth_idx)
+                cell.value = touch.method_counts[shorthand]
+                cell.alignment = centre_text
+
+    # === ROW/COLUMN SIZES ===
+    # Rows
+    sheet.row_dimensions[2].height = font_size * 6 # Title
+    sheet.row_dimensions[3].height = font_size * 4.5 # 'made by me'
+    for row in range(4, len(touches) + 5 + 1):
+        sheet.row_dimensions[row].height = font_size * 1.4
+    # Columns
+    vertical_text_column_width = 3
+    sheet.column_dimensions["B"].width = 6.5 # 'Length'
+    sheet.column_dimensions["C"].width = max_len((t.call_string for t in touches)) * 1.2 # 'Calling'
+    sheet.column_dimensions["D"].width = max_len((t.calling_position_string for t in touches)) * 1.2
+    sheet.column_dimensions["E"].width = 5 # 'Runs'
+    sheet.column_dimensions["F"].width = vertical_text_column_width # 'Queens'
+    sheet.column_dimensions["G"].width = vertical_text_column_width # 'Backrounds'
+    sheet.column_dimensions["H"].width = max((len(t.notes or "") for t in touches)) * 0.9 # 'Notes'
+    for method_idx in range(len(methods)):
+        col_name = get_column_letter(9 + method_idx)
+        sheet.column_dimensions[col_name].width = vertical_text_column_width
+
+    workbook.save(path)
+
+
+def max_len(strings: Iterable[str]) -> int:
+    """Returns the maximum length of an `Iterable` of strings"""
+    return max((len(s or "") for s in strings))
 
 #######################
 # PLACE NOTATION CODE #
